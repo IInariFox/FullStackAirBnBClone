750170ae798b93e0e9ec72fc6a5559e7241620c1
nathon <nathonchavez01@gamil.com>
2022-01-06 11:10:08 +0900

    Test checkpoint, rough draft

--- db/migrate/20190614042322_create_sessions.rb
+++ db/migrate/20190614042322_create_sessions.rb
@@ -1,4 +1,4 @@
-class CreateSessions < ActiveRecord::Migration[5.2]
+class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions do |t|
       t.string :token
--- db/migrate/20220104232314_create_properties.rb
+++ db/migrate/20220104232314_create_properties.rb
@@ -1,17 +1,17 @@
-class CreateProperties < ActiveRecord::Migration[5.2]
+class CreateProperties < ActiveRecord::Migration[6.0]
   def change
     create_table :properties do |t|
-      t.string :title
-      t.string :description
-      t.string :city
-      t.string :country
-      t.string :property_type
-      t.integer :price_per_night
+      t.string :title 
+      t.string :description 
+      t.string :city 
+      t.string :country 
+      t.string :property_type 
+      t.integer :price_per_night 
       t.integer :max_guests
-      t.integer :bedrooms
-      t.integer :beds
-      t.integer :baths
-      t.string :image_url
+      t.integer :bedrooms 
+      t.integer :beds 
+      t.integer :baths 
+      t.string :image_url 
       t.belongs_to :user, index: true, foreign_key: true
       t.timestamps
     end
